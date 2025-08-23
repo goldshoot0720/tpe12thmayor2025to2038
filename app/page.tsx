f25ba49051263fcb7e34a749494d1f098734a38c
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-23 18:35:59 +0800

    20250823 v1

---
+++ app/page.tsx
@@ -0,0 +1,68 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>黃&nbsp;</span>
+        <span className={title({ color: "violet" })}>鋒兄&nbsp;</span>
+        <br />
+        <span className={title()}>
+          第12屆台北市長候選人
+          <br />
+          第12屆台北市代理市長
+          <br />
+          第13屆台北市長候選人
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
+          第8屆→高考三級資訊處理榜首
+          <br />
+          第9屆→……
+          <br />
+          第10屆→……
+          <br />
+          第11屆→台北市副市長→第12屆台北市長候選人
+          <br />
+          第12屆→台北市副市長→第12屆台北市代理市長→第13屆台北市長候選人
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
