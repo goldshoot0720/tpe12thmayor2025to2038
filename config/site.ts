4eda9771a77be5b8c8dcc99e400cb078563b117f
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-23 23:51:56 +0800

    20250823 v3

--- config/site.ts
+++ config/site.ts
@@ -48,10 +48,10 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
+    github: "https://github.com",
+    twitter: "https://twitter.com",
     docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    // sponsor: "https://patreon.com/jrgarciadev",
+    discord: "https://discord.gg",
+    sponsor: "https://patreon.com",
   },
 };
