b21c397c1c3419105e3d690a15f20e4173001674
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-25 00:39:48 +0800

    20250825 v1

--- azuredevops-git.bat
+++ azuredevops-git.bat
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "20250825 v1"
 git remote add origin https://chbondg@dev.azure.com/chbondg/tpe12thmayor2025to2038/_git/tpe12thmayor2025to2038.git
-git checkout -b main
+git checkout -b all
 git push -u origin --all
 pause
