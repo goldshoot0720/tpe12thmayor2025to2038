4dc49a8205697ce069e9013a3f3fe6f67329b490
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-25 23:21:01 +0800

    20250825 v2

--- tpe12thmayor2025to2038-git.bat
+++ tpe12thmayor2025to2038-git.bat
@@ -1,11 +1,7 @@
 cd "C:\Users\chbon\Documents\tpe12thmayor2025to2038"
 git init
 git add .
-<<<<<<< HEAD
-git commit -m "20250824 v6"
-=======
-git commit -m "20250825 v1"
->>>>>>> 8c23f66 (20250825 v1)
+git commit -m "20250825 v2"
 git remote add origin https://github.com/goldshoot0720/tpe12thmayor2025to2038.git
 git checkout -b main
 git push -u origin main
